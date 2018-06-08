8203f30f7aca8af9b8717b901898508177cd7d9b
gonggongjohn <gonggongjohn@163.com>
2018-06-08 22:30:50 +0800

    Refresh Block Structure

---
+++ src/main/java/manor/tile/machine/TileJuiceExtractor.java
@@ -0,0 +1,20 @@
+package manor.tile.machine;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileJuiceExtractor extends TileEntity {
+    public TileJuiceExtractor() {
+
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
+        return super.writeToNBT(nbt);
+    }
+}
